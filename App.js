6c62c7204b350afda06b2da59932c2af62854ca4
yymin1022 <yymin1022@gmail.com>
2022-12-04 22:49:09 +0900

    Update App.js : Added Base UI Widgets

--- App.js
+++ App.js
@@ -1,17 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
+  Button,
   SafeAreaView,
-  ScrollView,
   StatusBar,
+  Switch,
   Text,
   useColorScheme,
   View,
 } from 'react-native';
 
-import {Colors, Header} from 'react-native/Libraries/NewAppScreen';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
 
 const App: () => Node = () => {
   const isDarkMode = useColorScheme() === 'dark';
+  const [checked, setChecked] = useState(true);
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
@@ -23,16 +25,36 @@ const App: () => Node = () => {
         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
         backgroundColor={backgroundStyle.backgroundColor}
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <View
+      <View
+        style={{
+          backgroundColor: '#448AFF',
+          height: '7.5%',
+          width: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}>
+        <Text
           style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+            color: 'white',
+            fontSize: 20,
           }}>
-          <Text>Hello, World!</Text>
-        </View>
-      </ScrollView>
+          "GDSC CAU RN UI Test"
+        </Text>
+      </View>
+      <View
+        style={{
+          backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          height: '100%',
+          width: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}>
+        <Button title="Click Me!" onPress={() => alert('Button Test!')} />
+        <Switch value={checked} onValueChange={() => setChecked(!checked)} />
+      </View>
     </SafeAreaView>
   );
 };
